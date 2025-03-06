89554b82967c36d7978f79b56392faf38842cd8b
Jeff <jeff.wafula1@student.moringaschool.com>
2025-03-06 14:13:51 +0300

    after completion

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// saturdayFun function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // mondayWork function expression
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // wrapAdjective function
+  function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
